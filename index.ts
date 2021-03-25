46ef89a88acb8c9b6d360a292a12e2636da07b32
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-03-25 19:59:02 +0530

    Initialised the lexer clsss

--- index.ts
+++ index.ts
@@ -1,4 +1,12 @@
 import { argv } from 'process'
+import { readFile, existsSync } from 'fs'
+
+const createLongLexer = (filename) => {
+    const fileExists = existsSync(filename)
+    if(!fileExists){
+        return null;
+    }
+}
 
 class LongArgumentParser {
     private arguments:Array<string>
@@ -11,5 +19,11 @@ class LongArgumentParser {
 
     parseLongArguments = () => {
         if(this.length == 0){return null}
+        else {
+            if(this.arguments[0].endsWith(".long")){
+                const coderunner = createLongLexer(this.arguments[0])
+            }
+        }
+        return undefined
     }
 }
