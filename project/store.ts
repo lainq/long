b8777ceb2e1068a8ba787745057569da077ccee8
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-03-31 11:25:30 +0530

    Added some comments

--- project/store.ts
+++ project/store.ts
@@ -8,13 +8,31 @@ export class LongProjectStore {
     private readonly projectName:string
     private projectDirectory:string
 
+    // the path of store where the information about the 
+    // projects is stored
     public store:string = join(__dirname, "json", "store.json")
 
-    constructor(name, directory){
+    /**
+     * @constructor
+     * 
+     * @param {String} name The name of the project
+     * @param {String} directory The path of the project
+     */
+    constructor(name:string, directory:string){
         this.projectName = name;
         this.projectDirectory = directory
     }
 
+    /**
+     * @private
+     * 
+     * Takes in a filename and check if the file exists or not
+     * 
+     * @param filename The name of the file to check
+     * the existence
+     * @returns A boolean whether the file exists or
+     * not
+     */
     private checkFileExistence(filename:string):boolean {
         try{
             return existsSync(filename)
@@ -24,7 +42,17 @@ export class LongProjectStore {
         return false
     }
 
-    public storeProjectInformation = () => {
+    /**
+     * @public
+     * 
+     * Store the information about the project
+     * in the store file
+     * 
+     * Check if certain files or folders exist and
+     * then proceed to create them
+     * 
+     */
+    public storeProjectInformation = ():void => {
         const store = this.store
         if(!this.checkFileExistence(join(__dirname, "json"))){
             mkdirSync(join(__dirname, "json"))
@@ -36,7 +64,14 @@ export class LongProjectStore {
             }))
         }
         
+        // read the information from the file and parse
+        // it to JSON data
         let data = JSON.parse(readFileSync(store).toString())
+
+        //  Check if the dictionary has a key calles
+        // projects which is an array of all
+        // the projects created
+
         if(!Object.keys(data).includes("projects")){
             const exception = new LongException(
                 "Process aborted due to an internal error",
@@ -45,6 +80,8 @@ export class LongProjectStore {
             ).evokeLongException()
         }
 
+        // add the new project information
+        // to the dictionary
         data.projects.push({
             name : this.projectName,
             path : this.projectDirectory,
@@ -55,10 +92,28 @@ export class LongProjectStore {
 
     }
 
-    public writeFile = (store, data) => {
+    /**
+     * @public
+     * 
+     * Write information into a file
+     * 
+     * @param {String} store The location of the storage file
+     * to save the data
+     * @param {String} data The data to save inside of the file
+     */
+    public writeFile = (store:string, data:string):void => {
         writeFileSync(store, JSON.stringify(data))
     }
 
+    /**
+     * @private
+     * 
+     * Recieves a date as the parameter and 
+     * returns it as a string after formatting
+     * 
+     * @param {Date} date The date object to format as a string
+     * @returns Formatted date in the form of a string
+     */
     private createdAt = (date:Date):string => {
         let dateString = ""
         let values = [
@@ -75,6 +130,16 @@ export class LongProjectStore {
         return dateString
     }
 
+    /**
+     * @private
+     * 
+     * Checks if a file is empty or not
+     * 
+     * @param path The path of the file to check if
+     * empty
+     * @returns A boolean, whether the file is empty
+     * or not 
+     */
     private isFileEmpty = (path:string):boolean => {
         if(!this.checkFileExistence(path)){
             return false
