e9099f3e5482d23fb45d200e20b5ddf49423eb6b
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-03-31 10:33:16 +0530

    Creating the LongProjectStore

---
+++ project/store.ts
@@ -0,0 +1,14 @@
+
+export class LongProjectStore {
+    private readonly projectName:string
+    private projectDirectory:string
+
+    constructor(name, directory){
+        this.projectName = name;
+        this.projectDirectory = directory
+    }
+
+    public storeProjectInformation = () => {
+        
+    }
+}
