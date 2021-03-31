d453cf7f585f7c2f3a25ca8be18273077944be9c
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-03-31 15:42:41 +0530

    Added some comments

--- project/list.ts
+++ project/list.ts
@@ -1,21 +1,29 @@
 import { yellow } from "chalk";
 import { readFileSync, existsSync, writeFileSync } from "fs";
 import { join } from "path";
+import { table as Table } from 'table'
 
 
 export class LongProjectList {
     private readonly store:string;
 
+    /**
+     * @constructor
+     */
     constructor() {
         this.store = join(__dirname, "json", "store.json")
         const storeExists = this.checkFileExistence(this.store)
 
+        // Check if the store exists, if not
+        // no projects are created or saved
         if(!storeExists){
             console.log(yellow("It seems like you haven't created any project"))
             process.exit()
         }
 
         const data = readFileSync(this.store).toString()
+        // if the data is not convertable to json
+        // rewrite the file
         if(!this.convertToJson(data)){
             writeFileSync(
                 this.store,
@@ -29,6 +37,14 @@ export class LongProjectList {
         process.exit()
     }
 
+    /**
+     * @private
+     * 
+     * Throws all the projects in the project list
+     * 
+     * @param project All the projects
+     * @returns Dummy value
+     */
     private throwProjectList = (project:Array<Object>):boolean => {
         for(let projectIndex=0; projectIndex<project.length; projectIndex++){
             console.log(project)
@@ -36,6 +52,15 @@ export class LongProjectList {
         return true
     }
 
+    /**
+     * @private
+     * 
+     * Checks if a string can be parsed into
+     * json data without any errors
+     * 
+     * @param data The data to check the conversion
+     * @returns Returns a boolean
+     */
     private convertToJson = (data:string):boolean => {
           try {
             let json = JSON.parse(data);
@@ -45,6 +70,14 @@ export class LongProjectList {
           }
     }
 
+    /**
+     * @private
+     * 
+     * Check whether a file exists or not
+     * 
+     * @param filepath The path to check the existence
+     * @returns Whether the file exists or not
+     */
     private checkFileExistence = (filepath:string):boolean => {
         try {
             return existsSync(filepath)
